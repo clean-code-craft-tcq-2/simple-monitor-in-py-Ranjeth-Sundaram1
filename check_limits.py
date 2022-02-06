de7ad8f8f04eda8df74c8a4f7e6dd5a35b4e646e
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-06 15:04:50 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,33 +1,40 @@
+def DefineParametersRange():
+    parameter_range ={}
+    parameter_range['temperature']  = {min : 0, max : 45}
+    parameter_range['soc']          = {min : 20, max : 80}
+    parameter_range['charge_rate']  = {min : 0.8}
+    return parameter_range
+    
+def DefineParametersMinLimit():
+    min_temperature = 0
 
-def TemperatureIsNOK(temperature:float)->bool:
-    if temperature < 0 or temperature > 45:
+def TemperatureIsNOK(Parameter_range, temperature:float)->bool:
+    if temperature < Parameter_range['temperature']['min'] or temperature > Parameter_range['temperature']['max']:
         print('Temperature is out of range!')
         return False
-    else:
-        return True
+    return True
 
-def SocIsNOK(soc:float)->bool:
-    if soc < 20 or soc > 80:
+def SocIsNOK(Parameter_range, soc:float)->bool:
+    if soc < Parameter_range['soc']['min'] or soc > Parameter_range['soc']['max']:
         print('State of Charge is out of range!')
         return False
-    else:
-        return True
+    return True
 
-def ChargerateIsNOK(charge_rate:float)->bool:
-    if charge_rate > 0.8:
+def ChargerateIsNOK(Parameter_range, charge_rate:float)->bool:
+    if charge_rate > Parameter_range['charge_rate']['min']:
         print('Charge rate is out of range!')
         return False
-    else:
-        return True
+    return True
 
-def IsBatteryOK(temperature, soc, charge_rate)->bool:
-    if TemperatureIsNOK(temperature) and SocIsNOK(soc) and ChargerateIsNOK(charge_rate):
-        return True
-    else:
+def IsBatteryOK(Parameter_range, temperature, soc, charge_rate)->bool:
+    if TemperatureIsNOK(Parameter_range, temperature) or SocIsNOK(Parameter_range, soc) or ChargerateIsNOK(Parameter_range, charge_rate):
         return False
+    else:
+        return True
 
 if __name__ == '__main__':
-    assert(IsBatteryOK(25, 70, 0.7) is True)
-    assert(IsBatteryOK(50, 85, 0) is False)
-    assert(IsBatteryOK(-50, 85, 0) is False)
-    assert(IsBatteryOK(-50, 85, 0.9) is False)
+    Parameter_range = DefineParametersRange()
+    assert(IsBatteryOK(Parameter_range, 25, 70, 0.7) is True)
+    assert(IsBatteryOK(Parameter_range, 50, 85, 0) is False)
+    assert(IsBatteryOK(Parameter_range, -50, 85, 0) is False)
+    assert(IsBatteryOK(Parameter_range, -50, 85, 0.9) is False)
