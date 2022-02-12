c87cf02e3c01c1adc10345e054a5964c8e4c1c9a
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-13 02:56:41 +0530

    Changed the code to add extension

--- check_limits.py
+++ check_limits.py
@@ -1,44 +1,68 @@
-def DefineParametersRange():
-    parameter_range ={}
-    parameter_range['temperature']  = {'min' : 0, 'max' : 45}
-    parameter_range['soc']          = {'min' : 20, 'max' : 80}
-    parameter_range['charge_rate']  = {'max' : 0.8}
-    return parameter_range
+def DefineParametersInfo():
+    global defined_parameters_info
+    defined_parameters_info ={}
+    defined_parameters_info['Temperature']  = {'min' : 0, 'max' : 45, 'tolarance_in_percentage': 5}
+    defined_parameters_info['Soc']          = {'min' : 20, 'max' : 80, 'tolarance_in_percentage': 5}
+    defined_parameters_info['Charge Rate']  = {'trend_value' : 0.8, 'tolarance_in_percentage': 5}
+    return defined_parameters_info
 
-def TemperatureIsOK(temperature_range, temperature:float)->bool:
-    if temperature in range(temperature_range['min'], temperature_range['max'], ):
+def DefineAlertMessages():
+    alert_messages = {}
+    # 2: Out of Range , 1: approaching maximum tolarance , 0 : appraching minimum tolarance
+    alert_messages ['English'] = {2: "parameter is out of range",  1: "parameter approaching maximum range", 1: "parameter approaching minimum range"}
+    alert_messages['Spanish'] = {2: "el parámetro está fuera de rango", 1: "parámetro acercándose al rango máximo", 1: "parámetro acercándose al rango mínimo"}
+    alert_messages['German'] = {2: "Parameter ist außerhalb des gültigen Bereichs", 2: "Parameter nähert sich dem maximalen Bereich", 1: "Parameter nähert sich dem Mindestbereich"}
+    return alert_messages
+
+def DefineAlertMessagesForLanguage(choosen_language):
+    alert_messages = DefineAlertMessages()
+    return alert_messages[choosen_language]
+
+def IsParameterInRange(parameter, parameter_value, parameter_info, alert_messages) -> bool:
+    if parameter_value in range(parameter_info['min'], parameter_info['max'], ):
         return True
+    PrintAlertInConsole(parameter, alert_messages[2])
     return False
 
-def SocIsOK(soc_range, soc:float)->bool:
-    if soc in range (soc_range['min'], soc_range['max'], ):
-       return True
+def IsParameterInTrend(parameter, parameter_value, parameter_info, alert_messages) -> bool:
+    if parameter_value < parameter_info['trend_value']:
+        return True
+    PrintAlertInConsole(parameter, alert_messages[2])
     return False
 
-def ChargerateIsOK(charge_rate_range, charge_rate:float)->bool:
-    if charge_rate > charge_rate_range['max']:
-        return False
+def IsMinimumTolarenceCheckOK(parameter,parameter_value, parameter_info,alert_message) -> bool:
+    if parameter_value not in range(parameter_info['min'], (parameter_info['min']+parameter_info['min']*(parameter_info['tolarance_in_percentage']/100))):
+        return True
+    PrintAlertInConsole(parameter, alert_message[0])
+    return False
+
+def IsMaximumTolarenceCheckOK(parameter, parameter_value, parameter_info, alert__message) -> bool:
+    if parameter_value not in range(parameter_info['max'], (parameter_info['max']+parameter_info['max']*(parameter_info['tolarance_in_percentage']/100))):
+        return True
+    PrintAlertInConsole(parameter, alert__message[1])
+    return False
+
+def PrintAlertInConsole(parameter, alert_message):
+    print (f"{parameter} {alert_message}")
+
+def IsBatteryOK(InputParameterFromSensor, parameters_info, alert_messages)->bool:
+    battery_status_report = True
+    for parameter, parameter_value in InputParameterFromSensor.items():
+        if "trend_value" in parameters_info[parameter].keys():
+            battery_status_report = IsParameterInTrend(parameter, parameter_value, parameters_info[parameter], alert_messages)
+        else:
+            battery_status_report = IsParameterInRange(parameter, parameter_value, parameters_info[parameter], alert_messages)
+        if battery_status_report == False:
+            return False
+        else:
+            IsMinimumTolarenceCheckOK(parameter, parameter_value, parameters_info[parameter],alert_messages)
+            IsMaximumTolarenceCheckOK(parameter, parameter_value, parameters_info[parameter],alert_messages)
     return True
 
-def IsBatteryOK(Parameter_range, temperature, soc, charge_rate)->bool:
-    battery_status_report = [TemperatureIsOK(Parameter_range['temperature'], temperature), SocIsOK(Parameter_range['soc'], soc), ChargerateIsOK(Parameter_range['charge_rate'], charge_rate)]
-    if all(battery_status_report) is True:
-        return True
-    else:
-        return False
+def TestBatteryStatus(alert_messages, parameters_info):
+    parameters_info =  DefineParametersInfo()
+    assert(IsParameterInRange("Temperature", 40, parameters_info["Temperature"], alert_messages))
 
-def TestBatteryStatus(Parameter_range):
-    assert(TemperatureIsOK(Parameter_range['temperature'], 40) is True)
-    assert(TemperatureIsOK(Parameter_range['temperature'], 100) is False)
-    assert(SocIsOK(Parameter_range['soc'], 10) is False)
-    assert(SocIsOK(Parameter_range['soc'], 50) is True)
-    assert(ChargerateIsOK(Parameter_range['charge_rate'], 1) is False)
-    assert(ChargerateIsOK(Parameter_range['charge_rate'], 0.5) is True)
-    assert(IsBatteryOK(Parameter_range, 25, 70, 0.7) is True)
-    assert(IsBatteryOK(Parameter_range, 50, 85, 0) is False)
-    assert(IsBatteryOK(Parameter_range, -50, 85, 0) is False)
-    assert(IsBatteryOK(Parameter_range, -50, 85, 0.9) is False)
-    
 if __name__ == '__main__':
-    Parameter_range = DefineParametersRange()
-    TestBatteryStatus(Parameter_range)
+    alert_messages = DefineAlertMessagesForLanguage("English")
+    TestBatteryStatus(alert_messages)
