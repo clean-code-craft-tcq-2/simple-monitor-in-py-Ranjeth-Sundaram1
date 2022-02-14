ddc747e60eb821ea75cffb5569c8232db8b6bb72
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-14 22:36:13 +0530

    Create test_bms.py

---
+++ test_bms.py
@@ -0,0 +1,15 @@
+def TestBatteryStatus(alert_messages):
+    parameters_info =  DefineParametersInfo()
+    assert(IsParameterInRange("Temperature", 40, parameters_info["Temperature"], alert_messages) == True)
+    assert(IsParameterInRange("Soc", 100, parameters_info["Soc"], alert_messages) == False)
+    assert(IsBatteryOK({"Charge Rate": 1, "Temperature": 50}, parameters_info, alert_messages)==False)
+    assert(IsBatteryOK({"Temperature": 50}, parameters_info, alert_messages)==False)
+    assert(PrintAlertInConsole("Temperature", "parámetro acercándose al rango máximo") == "Warning: Temperature parámetro acercándose al rango máximo")
+    assert(IsMinimumTolarenceCheckOK("Temperature",44,parameters_info["Temperature"], alert_messages)==False)
+    assert(IsMinimumTolarenceCheckOK("Temperature",2,parameters_info["Temperature"], alert_messages)==True)
+    assert(IsMaximumTolarenceCheckOK("Soc", 77.5, parameters_info["Soc"], alert_messages)==True)
+    assert(IsMaximumTolarenceCheckOK("Soc", 77.5, parameters_info["Soc"], alert_messages)==True)
+
+if __name__ == '__main__':
+    alert_messages = DefineAlertMessagesForLanguage("English")
+    TestBatteryStatus(alert_messages)
