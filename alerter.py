e5927496e4641757903131ab1823b1a9571e84d2
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-14 23:22:41 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -1,3 +1,12 @@
+import check_limits
 def PrintAlertInConsole(parameter, alert_message):
     print (f"Warning: {parameter} {alert_message}")
     return f"Warning: {parameter} {alert_message}"
+
+def GenerateAlertMessageIfRequired(parameter, parameter_value, parameter_info,alert_messages):
+    if parameter_info["alert"] == True:
+        check_limits.IsMinimumTolarenceCheckOK(parameter, parameter_value, parameter_info,alert_messages)
+        check_limits.IsMaximumTolarenceCheckOK(parameter, parameter_value, parameter_info,alert_messages)  
+        check_limits.IsTrendTolaranceCheckOK(parameter, parameter_value, parameter_info,alert_messages)  
+    else:
+        pass
