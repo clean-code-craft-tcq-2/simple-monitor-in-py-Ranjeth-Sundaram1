f94629060c6d0a8abe51911dd7324cb0eb1fd75d
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-14 23:19:32 +0530

    Update test_bms.py

--- test_bms.py
+++ test_bms.py
@@ -5,7 +5,7 @@ def TestBatteryStatus(alert_messages):
     parameters_info =  define_defalut_value.DefineParametersInfo()
     assert(check_limit.IsParameterInRange("Temperature", 40, parameters_info["Temperature"], alert_messages) == True)
     assert(check_limit.IsParameterInRange("Soc", 100, parameters_info["Soc"], alert_messages) == False)
-    assert(check_limit.IsBatteryOK({"Charge Rate": 1, "Temperature": 50}, parameters_info, alert_messages)==False)
+    assert(check_limit.IsBatteryOK({"Charge Rate": 1, "Temperature": 50, "Soc":77.5}, parameters_info, alert_messages)==False)
     assert(check_limit.IsBatteryOK({"Temperature": 50}, parameters_info, alert_messages)==False)
     assert(check_limit.PrintAlertInConsole("Temperature", "parámetro acercándose al rango máximo") == "Warning: Temperature parámetro acercándose al rango máximo")
     assert(check_limit.IsMinimumTolarenceCheckOK("Temperature",44,parameters_info["Temperature"], alert_messages)==False)
