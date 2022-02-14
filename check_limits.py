dc0419050017b84b7e0de2bfa9fe7ea14494e695
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-14 22:49:27 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,3 +1,5 @@
+import alerter
+
 def IsParameterInRange(parameter, parameter_value, parameter_info, alert_messages) -> bool:
     if parameter_value in range(parameter_info['min'], parameter_info['max'], ):
         return True
@@ -11,18 +13,14 @@ def IsParameterInTrend(parameter, parameter_value, parameter_info, alert_message
 def IsMinimumTolarenceCheckOK(parameter,parameter_value, parameter_info,alert_message) -> bool:
     if parameter_value > float(parameter_info['min']) and parameter_value < (parameter_info['min']+parameter_info['max']*(parameter_info['tolarance_in_percentage']/100)):
         return True
-    PrintAlertInConsole(parameter, alert_message[0])
+    alerter.PrintAlertInConsole(parameter, alert_message[0])
     return False
 
 def IsMaximumTolarenceCheckOK(parameter, parameter_value, parameter_info, alert__message) -> bool:
     if parameter_value > (parameter_info['max']-parameter_info['max']*(parameter_info['tolarance_in_percentage']/100)) and parameter_value < float(parameter_info['max']):
         return True
-    PrintAlertInConsole(parameter, alert__message[1])
+    alerter.PrintAlertInConsole(parameter, alert__message[1])
     return False
-
-def PrintAlertInConsole(parameter, alert_message):
-    print (f"Warning: {parameter} {alert_message}")
-    return f"Warning: {parameter} {alert_message}"
 
 def GenerateAlertMessage(parameter, parameter_value, parameter_info,alert_messages):
     if parameter_info["alert"] == True:
