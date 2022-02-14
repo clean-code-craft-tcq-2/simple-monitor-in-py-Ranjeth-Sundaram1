d142d0ac52d073d647f56635d26ca44adb2a0229
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-15 00:20:19 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -1,8 +1,15 @@
 import check_limits
-def PrintAlertInConsole(parameter, alert_message):
-    print (f"Warning: {parameter} {alert_message}")
-    return f"Warning: {parameter} {alert_message}"
 
+def PrintWarningInConsole(param_in_range, parameter, alert_message):
+    if param_in_range :
+        print (f"Warning: {parameter} {alert_message}")
+        return f"Warning: {parameter} {alert_message}"
+    else:
+        return(PrintAlertInConsole(parameter))
+
+def PrintAlertInConsole(parameter):
+    print(f"Alert: {parameter} value out of range")
+    return f"Alert: {parameter} value out of range"
 def GenerateAlertMessageIfRequired(parameter, parameter_value, parameter_info,alert_messages):
     if parameter_info["alert"] == True:
         check_limits.IsMinimumTolarenceCheckOK(parameter, parameter_value, parameter_info,alert_messages)
